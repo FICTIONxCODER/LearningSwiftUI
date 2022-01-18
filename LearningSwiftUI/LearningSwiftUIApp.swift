06ef4d1307fcc16f3f24601bd6ec8297def24f8e
Abhishek <abhishek.chaudhary@beasensors.com>
2022-01-18 18:42:37 +0530

    Initial Commit

---
+++ LearningSwiftUI/LearningSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningSwiftUIApp.swift
+//  LearningSwiftUI
+//
+//  Created by abhishek chaudhary on 18/01/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
